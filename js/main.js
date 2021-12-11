e7efbc4c6daf47e9b8332b8f7bb6129058f0df8c
Kukuh Rahmadani <krahmadani1@gmail.com>
2021-12-11 17:39:43 +0700

    Countdown

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate = '2021/08/01';
+        var finalDate = '2022/01/10';
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
