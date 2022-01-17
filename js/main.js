2be0b070ccd1935bca2af87e9c5480d876599cfc
Kukuh Rahmadani <44625292+KukuhKKH@users.noreply.github.com>
2022-01-17 14:27:31 +0700

    Tunda Duls Gayn, belum semvat

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate = '2022/01/10';
+        var finalDate = '2022/03/01';
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
